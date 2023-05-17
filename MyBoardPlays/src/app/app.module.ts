23529b0e39f59b082bcef51d7584ffcf9fa465fc
Daniel <danijb_10@hotmail.com>
2023-05-17 20:53:26 +0200

    modificado herramientas acabado html, falta arreglar tema ngmodel para que haga las funciones bien

--- MyBoardPlays/src/app/app.module.ts
+++ MyBoardPlays/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
--- MyBoardPlays/src/app/herramientas/herramientas.component.ts
+++ MyBoardPlays/src/app/herramientas/herramientas.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'app-herramientas',
   templateUrl: './herramientas.component.html',
@@ -7,4 +8,71 @@ import { Component } from '@angular/core';
 })
 export class HerramientasComponent {
   num_dados: number;
+  num_caras: number;
+  resultados: number[];
+  sumaTotal: number;
+  numParticipantes : number;
+  numPremiado: number;
+  horas: number;
+  minutos: number;
+  segundos: number;
+  tiempo: string;
+  intervalo: any;
+  
+
+  lanzarDados() {
+    let resultados = [];
+    for (let i = 0; i < this.num_dados; i++) {
+      let resultado = Math.floor(Math.random() * this.num_caras) + 1;
+      resultados.push(resultado);
+    }
+    return resultados;
+  }
+
+  ejecutarLanzamiento() {
+    this.resultados = this.lanzarDados();
+    this.sumaTotal = this.resultados.reduce((a, b) => a + b, 0);
+  }
+
+  ejecutarGiro() {
+    this.numPremiado = Math.floor(Math.random() * this.numParticipantes) + 1;
+    }
+
+    constructor() {
+      this.horas = 0;
+      this.minutos = 0;
+      this.segundos = 0;
+      this.tiempo = '';
+      this.intervalo = null;
+    }
+  
+    iniciarCronometro() {
+      if (this.intervalo) {
+        clearInterval(this.intervalo);
+      }
+  
+      this.intervalo = setInterval(() => {
+        this.actualizarTiempo();
+      }, 1000);
+    }
+  
+    actualizarTiempo() {
+      if (this.segundos < 59) {
+        this.segundos++;
+      } else {
+        this.segundos = 0;
+        if (this.minutos < 59) {
+          this.minutos++;
+        } else {
+          this.minutos = 0;
+          this.horas++;
+        }
+      }
+  
+      this.tiempo = `${this.horas.toString().padStart(2, '0')}:
+                     ${this.minutos.toString().padStart(2, '0')}:
+                     ${this.segundos.toString().padStart(2, '0')}`;
+    }
 }
+
+
