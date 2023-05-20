a2d37f2546750b783b7c2a5f69970dd59bc813b7
Daniel <danijb_10@hotmail.com>
2023-05-20 20:11:10 +0200

    lanzar dados y ruleta funcionando, falta timer

--- MyBoardPlays/src/app/herramientas/herramientas.component.ts
+++ MyBoardPlays/src/app/herramientas/herramientas.component.ts
@@ -1,78 +1,62 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-herramientas',
   templateUrl: './herramientas.component.html',
   styleUrls: ['./herramientas.component.css']
 })
 export class HerramientasComponent {
-  num_dados: number;
-  num_caras: number;
+
+  
+  numeroDados: number;
+  numeroCaras: number;
   resultados: number[];
   sumaTotal: number;
   numParticipantes : number;
   numPremiado: number;
-  horas: number;
-  minutos: number;
-  segundos: number;
-  tiempo: string;
+  tiempo= '';
+  horas = 0;
+  minutos =0;
+  segundos=0;
   intervalo: any;
 
 
   lanzarDados() {
-    let resultados = [];
-    for (let i = 0; i < this.num_dados; i++) {
-      let resultado = Math.floor(Math.random() * this.num_caras) + 1;
-      resultados.push(resultado);
-    }
-    return resultados;
+    this.resultados = Array.from({ length: this.numeroDados }, () => Math.floor(Math.random() * this.numeroCaras) + 1);
   }
 
   ejecutarLanzamiento() {
-    this.resultados = this.lanzarDados();
+    this.lanzarDados();
     this.sumaTotal = this.resultados.reduce((a, b) => a + b, 0);
   }
 
   ejecutarGiro() {
     this.numPremiado = Math.floor(Math.random() * this.numParticipantes) + 1;
+  }
+
+  iniciarCronometro() {
+    if (this.intervalo) {
+      clearInterval(this.intervalo);
     }
 
-    constructor() {
-      this.horas = 0;
-      this.minutos = 0;
+    this.intervalo = setInterval(() => {
+      this.actualizarTiempo();
+    }, 1000);
+  }
+
+  actualizarTiempo() {
+    this.segundos++;
+    if (this.segundos === 60) {
       this.segundos = 0;
-      this.tiempo = '';
-      this.intervalo = null;
-    }
-
-    iniciarCronometro() {
-      if (this.intervalo) {
-        clearInterval(this.intervalo);
+      this.minutos++;
+      if (this.minutos === 60) {
+        this.minutos = 0;
+        this.horas++;
       }
-
-      this.intervalo = setInterval(() => {
-        this.actualizarTiempo();
-      }, 1000);
     }
 
-    actualizarTiempo() {
-      if (this.segundos < 59) {
-        this.segundos++;
-      } else {
-        this.segundos = 0;
-        if (this.minutos < 59) {
-          this.minutos++;
-        } else {
-          this.minutos = 0;
-          this.horas++;
-        }
-      }
-
-      this.tiempo = `${this.horas.toString().padStart(2, '0')}:
-                     ${this.minutos.toString().padStart(2, '0')}:
-                     ${this.segundos.toString().padStart(2, '0')}`;
-    }
+    this.tiempo = `${this.horas.toString().padStart(2, '0')}:
+                   ${this.minutos.toString().padStart(2, '0')}:
+                   ${this.segundos.toString().padStart(2, '0')}`;
+  }
 }
-
-
