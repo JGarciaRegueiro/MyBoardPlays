b953187cf4fe6a5f92caed55eddd77ab6ead035a
Marta Aparicio <mam3192@gmail.com>
2023-05-25 22:30:27 +0200

    validaciones login

--- MyBoardPlays/src/app/login/login.component.ts
+++ MyBoardPlays/src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
-import { NgForm } from '@angular/forms';
-import { Credentials } from '../model';
+import { FormControl, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { __values } from 'tslib';
+import { Credentials } from '../model';
+
 
 @Component({
   selector: 'app-login',
@@ -12,21 +13,27 @@ import { __values } from 'tslib';
 })
 export class LoginComponent {
 
-  creds: Credentials = {
-    email: '',
-    pass: ''
-  };
+  emailControl: FormControl;
+  passControl: FormControl;
 
   constructor(
     private apiService: ApiService,
     private router: Router
   ) {
-
+    this.emailControl = new FormControl('', [Validators.required, Validators.email]);
+    this.passControl = new FormControl('', [Validators.required]);
   }
 
-  login(form: NgForm){
+  login() {
+    if (this.emailControl.invalid || this.passControl.invalid) {
+      return;
+    }
 
-    this.apiService.login(this.creds)
+    const creds: Credentials = {
+      email: this.emailControl.value,
+      pass: this.passControl.value
+    };
+    this.apiService.login(creds)
       .subscribe(response => {
         this.router.navigate(['/'])
       })
