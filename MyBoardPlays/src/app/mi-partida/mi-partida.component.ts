364ea759e063f829eea6455797a1d78f7fee48a0
DanielJimenezBallesteros <danijb_10@hotmail.com>
2023-05-26 21:22:01 +0200

    cambios peticion y formulario partida

--- MyBoardPlays/src/app/mi-partida/mi-partida.component.ts
+++ MyBoardPlays/src/app/mi-partida/mi-partida.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Jugador } from '../jugador';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @Component({
   selector: 'app-mi-partida',
@@ -11,34 +12,72 @@ export class MiPartidaComponent implements OnInit {
   
   participantes: number;
   jugadores: Jugador[] = [];
-  fechaActual = new Date().toISOString().split('T')[0];
-  constructor(private route: ActivatedRoute) {}
+  fechaActual: string;
+  nombreJuego: string;
+  creadorPartida: string;
+  ubicacionPartida: string;
+  fechaEscogida: Date;
+  duracion: number;
+
+
+
+  nombre: string;
+  puntuacion: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
+    this.fechaActual = new Date().toISOString().split('T')[0]; // Asignar la fecha actual
     this.route.queryParams.subscribe(params => {
       this.participantes = Number(params['participantes']);
       this.generarFilas();
     });
   }
-  
 
   generarFilas() {
     this.jugadores = [];
     for (let i = 0; i < this.participantes; i++) {
-      const jugador = {
+      const jugador: Jugador = {
         nombre: '',
         email: '',
         ganador: false,
-        puntuaciones:0,
-        orden:0,
+        puntuaciones: 0,
+        orden: 0,
       };
       this.jugadores.push(jugador);
     }
   }
 
   agregarJugador() {
-    this.jugadores.push({ nombre: '', email: '', ganador: false , puntuaciones:0, orden:0});
+    this.jugadores.push({ nombre: '', email: '', ganador: false, puntuaciones: 0, orden: 0 });
   }
   
+  guardarPartida() {
+    const partida = {
+      nombreJuego: this.nombreJuego,
+      creadorPartida: this.creadorPartida,
+      ubicacionPartida: this.ubicacionPartida,
+      fechaEscogida: this.fechaEscogida,
+      duracion: this.duracion,
+    };
 
+    this.http.post('/apirest/partida/alta', partida).subscribe(
+      (response) => {
+        console.log('Partida guardada correctamente');
+        // Aquí puedes realizar alguna acción adicional después de guardar la partida
+      },
+      (error) => {
+        console.error('Error al guardar la partida:', error);
+        // Manejo de errores
+      }
+    );
+  }
+  
 }
+
+
+
+
