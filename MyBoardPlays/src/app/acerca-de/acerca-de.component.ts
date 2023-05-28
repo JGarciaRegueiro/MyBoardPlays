3e76e0bf94404551d098474521723c9a82426ff4
DanielJimenezBallesteros <danijb_10@hotmail.com>
2023-05-28 11:52:17 +0200

    acerca-de responsive

--- MyBoardPlays/src/app/acerca-de/acerca-de.component.ts
+++ MyBoardPlays/src/app/acerca-de/acerca-de.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-acerca-de',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AcercaDeComponent {
 
+  correo: string;
+  comentario: string;
+  enviado: string;
+  constructor(private http: HttpClient) {}
+
+  enviarCorreo() {
+    this.enviado ="Email enviado";
+    const datos = {
+      email: this.correo,
+      mensaje: this.comentario
+    };
+
+    this.http.post('/acerca-de/enviar-correo', datos).subscribe(
+      (response) => {
+        console.log('Correo electrónico enviado correctamente');
+        // Realiza alguna acción adicional después de enviar el correo
+      },
+      (error) => {
+        console.error('Error al enviar el correo electrónico:', error);
+        // Manejo de errores
+      }
+    );
+  }
 }
