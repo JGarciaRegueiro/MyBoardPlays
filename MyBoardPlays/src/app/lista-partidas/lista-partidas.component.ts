d039a18f2161d52f8871974968d604a6bf5acf7b
Marta Aparicio <mam3192@gmail.com>
2023-05-28 11:22:26 +0200

    mejorada tabla lista partidas

--- MyBoardPlays/src/app/lista-partidas/lista-partidas.component.ts
+++ MyBoardPlays/src/app/lista-partidas/lista-partidas.component.ts
@@ -1,31 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-lista-partidas',
   templateUrl: './lista-partidas.component.html',
-  styleUrls: ['./lista-partidas.component.css']
+  styleUrls: ['./lista-partidas.component.css'],
 })
-export class ListaPartidasComponent implements OnInit{
+export class ListaPartidasComponent implements OnInit {
+  pageActual: number = 1;
+  parameter1 = 'id';
+  parameter2 = 'asc';
+  filterPartidas: String = '';
 
-  pageActual:number =1;
- ngOnInit(): void {
+  partidas:string = ''
 
- }
+  ngOnInit(): void {}
 
-  private obtenerPartidas(){
+  private obtenerPartidas() {}
 
+  eliminarPartida() {}
 
-   }
+  verDetallesPartida() {}
 
-  eliminarPartida(id:number){
+  editarPartida() {}
 
+  exportExcel(): void {
+    const data: any[][] = [
+      ['ID', 'CREADOR', 'UBICACION', 'FECHA', 'ID JUEGO', 'DURACION'],
+      /*
+      ...this.partidas.map((partidas) => [
+        juego.id,
+        juego.nombre,
+        juego.descripcion,
+        juego.minParticipantes,
+        juego.maxParticipantes,
+        juego.dificultad,
+      ]),
+      */
+    ];
+
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Hoja1');
+
+    // Generar el archivo Excel
+    XLSX.writeFile(wb, 'lista_partidas.xlsx');
   }
-
-   verDetallesPartida(id:number){
-
-   }
-
-   editarPartida(){
-
-   }
 }
