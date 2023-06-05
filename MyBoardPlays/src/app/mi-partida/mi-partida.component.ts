008a4bf9bd6e03a7274b6d5c18511356ccc61f80
Marta Aparicio <mam3192@gmail.com>
2023-06-05 19:45:38 +0200

    mensaje exito y redirige a partidas

--- MyBoardPlays/src/app/mi-partida/mi-partida.component.ts
+++ MyBoardPlays/src/app/mi-partida/mi-partida.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JuegosService } from '../juegos.service';
 import { Juego } from '../juego';
@@ -8,6 +8,7 @@ import { ApiService } from '../api.service';
 import { Usuario } from '../usuario';
 import { Partida } from '../partida';
 import { PartidasService } from '../partidas.service';
+import swal from 'sweetalert2';
 
 
 @Component({
@@ -49,7 +50,8 @@ export class MiPartidaComponent implements OnInit {
     private http: HttpClient,
     private juegosServicio: JuegosService,
     private partidasServicio: PartidasService,
-    private usuarioServicio: ApiService
+    private usuarioServicio: ApiService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -116,6 +118,13 @@ export class MiPartidaComponent implements OnInit {
       this.partidasServicio.guardarNuevaPartida(partida).subscribe(
         dato => {
           console.log(dato);
+
+          // Mostrar SweetAlert y redirigir a la lista de partidas
+          swal('Partida Actualizada', `La partida ha sido actualizada con éxito`, 'success')
+            .then(() => {
+              // Redirigir a la lista de partidas
+              this.router.navigate(['/lista-partidas']);
+            });
         },
         error => {
           console.log(error);
